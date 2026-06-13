6098201ca8670c7932f7e346da59124b754ea292
SyedNoorAliShah <noorashah61@gmail.com>
2026-06-14 02:54:42 +0500

    switch to AWS - terraform and github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,21 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "azurerm_resource_group" "exam_rg" {
-  name     = "devops-exam-rg"
-  location = "East US"
+resource "aws_ecr_repository" "app_repo" {
+  name         = "my-web-app"
+  force_delete = true
 }
 
-resource "azurerm_container_group" "exam_app" {
-  name                = "my-web-app"
-  location            = azurerm_resource_group.exam_rg.location
-  resource_group_name = azurerm_resource_group.exam_rg.name
-  os_type             = "Linux"
-  ip_address_type     = "Public"
-  dns_name_label      = "my-web-app-noor"
-
-  container {
-    name   = "myapp"
-    image  = "nooralishah270/my-web-app:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 3000
-      protocol = "TCP"
-    }
-  }
+resource "aws_ecs_cluster" "app_cluster" {
+  name = "my-web-app-cluster"
 }
